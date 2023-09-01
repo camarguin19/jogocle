4927d6f431fb9cecab9e31f5671dd297f916378b
camarguin19 <126774745+camarguin19@users.noreply.github.com>
2023-09-01 11:10:02 -0300

    Add files via upload

---
+++ sistema/cadastro.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro do usuário</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+    <body class= "bg-secondary ">
+        <nav class="navbar bg-primary-subtle">
+            <div class="container-fluid">
+            
+            </div>
+        </nav>
+
+    <div class= "container text-center mt-5">
+            <h2>Cadastro do Usuário</h2>
+            <p>Preencha as informções abaixo e clique no botão cadastrar para criar seu cadatro.
+            </p>
+        <form action="" method="post">
+            <div class= "row">
+                <div class= "col-md-2">
+                    <input class= "form-control" type="text" name="" id="" placeholder= "Nome">
+                </div>
+                <div class= "col-md-2">
+                    <input class= "form-control" type="text" name="" id="" placeholder= "Sobrenome">
+                </div>
+                
+            </div>
+                </div>  
+                <div class= "col-md-2">
+                    <input class= "form-control" type="date" name="" id="" placeholder= "Data de Nascimento">
+                </div> 
+            </div> 
+            <div class= "row mt-3"> 
+                <div class= "col-md-5">
+                    <input class="form-control" type="email" name="" id="" placeholder= "email">
+            </div>
+                <div class= "col-md-3">
+                    <input class="form-control" type="password" name="" id="" placeholder= "Senha">
+                </div>
+            <div class= "row mt-3">
+                <div class="col-md-3">
+                    <select class="form-control" name="sexo" id="">
+                        <option value="">Selecione</option>
+                        <option value="Fem">Feminino</option>
+                        <option value="Masc">Masculino</option>
+                        <option value="Indeterminado">Prefiro não dizer</option>
+                    </select>
+                </div>
+                <div class="row mt-2">
+                    <div class="col-md-3">
+                        <input class="form-control" type="number" name="" id="" placeholder= "RG">
+                    </div>
+                </div>
+
+
+            </div>
+                <div class= "mt-3">
+                <input class= "btn btn-success btn-lg" type="submit" value="Cadastrar">
+                <a class= "btn btn-dark btn-lg" href="index.php">Já tenho uma conta</a>
+                </div>
+            </form>
+
+        </div>
+   
+        <footer class="footer py-3 bg-dark fixed-bottom">
+            <div class= "container text-center">
+
+                <p class="text-light">Todos os direitos reservados - 2023</p>
+
+            </div>
+
+        </footer>
+    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ sistema/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body class= "bg-dark-subtle">
+    <div class="container text-center mt-3 mb-1">
+        <img src="recursos\joguinho.png" alt="" width="600px">
+    <div>
+    <div class="container text-center mb-5">
+        <h2 class="text-light">Soulful Memories</h2>
+    </div>
+
+
+    <div class="container text-center mt-5">
+        <form action="" method="post">
+        <label class="form-label" for="usuario">Usuário</label>
+        <input class="form-control" type="email" name="user" id="usuario">
+     
+    
+        <!-- </div class= "row">
+            <div class="col-md-4"> -->
+
+                <label class="form-label" for="senha">Senha</label>
+
+                <input class="form-control" type="password" name="senha" id="senha">
+
+             
+                <input class="btn btn-success" type="submit" value="Verificar">
+            <!-- </div>
+            </div>
+ -->
+            <a  class="btn btn-dark" href="cadastro.php">Cadastrar</a>
+        </form>
+    </div>
+
+    <footer class="footer py-3 bg-dark fixed-bottom">
+        <div class= "container text-center">
+
+         <p class="text-light">Todos os direitos reservados - 2023</p>
+
+        </div>
+
+
+
+
+    </footer>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+
+
+</body>
+
+
+
+
+
+
+
+
+
+
+
+</html>
+
+ 
